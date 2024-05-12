5b48faedc240d4e2144e61ebc422e38898236579
Farid Andika <faridandika159@gmail.com>
2024-05-12 17:37:54 +0700

    Initial Commit

---
+++ machine-learning/machine_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  machine_learningApp.swift
+//  machine-learning
+//
+//  Created by Farid Andika on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct machine_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
